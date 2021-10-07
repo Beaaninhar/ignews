9001c9c1add97d3d08a77f929f191e8d76d8ac1a
ana.beatriz <ana.beatriz@m4u.com.br>
2021-10-07 18:16:30 -0300

    Pagamento com Stripe

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,5 +1,5 @@
 
-import { signIn, signOut, useSession } from 'next-auth/client';
+import { signIn, useSession } from 'next-auth/client';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
 import styles from './styles.module.scss';
@@ -12,23 +12,26 @@ export function SubscribeButton({priceId}: SubscribeButtonProps) {
     const session = useSession();
 
     async function handleSubscribe() {
+
         if(!session) {
             signIn('github');
             return;
         }
 
         try {
-            const response = await api.post('/subscribe');
+            const response = await api.post("/subscribe");
 
             const { sessionId } = response.data;
 
             const stripe = await getStripeJs();
 
             await stripe.redirectToCheckout({ sessionId });
-        } catch (error) {
-            console.log(error)
-            alert(error.message);
+            
+        }catch (err) {
+            alert(err.message);
         }
+
+        
     }
 
     return (
--- ignews/src/pages/api/subscribe.ts
+++ ignews/src/pages/api/subscribe.ts
@@ -1,22 +1,24 @@
 /* eslint-disable import/no-anonymous-default-export */
-import { query as q } from 'faunadb';
-import { NextApiRequest, NextApiResponse } from "next";
-import { getSession } from "next-auth/client";
-import { fauna } from '../../services/fauna';
-import { stripe } from "../../services/stripe";
+import { NextApiRequest, NextApiResponse } from "next"
+import { query as q } from 'faunadb'
+import { getSession } from 'next-auth/client'
+import { stripe } from '../../services/stripe'
+import { fauna } from "../../services/fauna"
+import { SSL_OP_SINGLE_DH_USE } from "constants"
 
 type User = {
     ref: {
         id: string;
     }
     data: {
-        stripe_customer_id: string;
+        stripe_customer_id: string
     }
 }
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method === 'POST') {
-        const session = await getSession({ req })
+
+        const session = await getSession({ req });
 
         const user = await fauna.query<User>(
             q.Get(
@@ -27,10 +29,10 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
             )
         )
 
-        let customerId = user.data.stripe_customer_id;
+        let customerId = user.data.stripe_customer_id
 
-        if (!customerId) {
-            const stripeCustomer = await stripe.customers.create({
+        if ( !customerId ) {
+            const stripeCustumer = await stripe.customers.create({
                 email: session.user.email,
                 // metadata
             })
@@ -40,31 +42,36 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
                     q.Ref(q.Collection('users'), user.ref.id),
                     {
                         data: {
-                            stripe_customer_id: stripeCustomer.id,
+                            stripe_customer_id: stripeCustumer.id,
                         }
                     }
                 )
             )
 
-            customerId = stripeCustomer.id;
+            customerId = stripeCustumer.id
         }
 
-        const stripeCheckoutSession = await stripe.checkout.sessions.create({
+        const stripeSheckoutSession = await stripe.checkout.sessions.create({
             customer: customerId,
             payment_method_types: ['card'],
             billing_address_collection: 'required',
             line_items: [
-                { price: 'price_1JhNJqKRNMIVvuj4Vm4ZSr5L', quantity: 1 }
+                {
+                    price: 'price_1JhNJqKRNMIVvuj4Vm4ZSr5L', quantity: 1
+                }
             ],
             mode: 'subscription',
             allow_promotion_codes: true,
             success_url: process.env.STRIPE_SUCCESS_URL,
             cancel_url: process.env.STRIPE_CANCEL_URL
+
         })
 
-        return res.status(200).json({ sessionId: stripeCheckoutSession.id })
+        return res.status(200).json({ sessionId: stripeSheckoutSession.id })
+
     } else {
-        res.setHeader('Allow', 'POST')
+        res.setHeader('Allow', 'POST    ')
         res.status(405).end('Method not allowed')
     }
+
 }
