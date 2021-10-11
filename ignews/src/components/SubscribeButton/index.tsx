84a247a7806d51e55f194014a49453e41378c274
ana.beatriz <ana.beatriz@m4u.com.br>
2021-10-11 18:00:30 -0300

    Feito modulo

--- ignews/src/components/SubscribeButton/index.tsx
+++ ignews/src/components/SubscribeButton/index.tsx
@@ -1,46 +1,47 @@
-
-import { signIn, useSession } from 'next-auth/client';
+import { useSession, signIn } from 'next-auth/client'
+import { useRouter } from 'next/router';
 import { api } from '../../services/api';
 import { getStripeJs } from '../../services/stripe-js';
-import styles from './styles.module.scss';
+import styles from './styles.module.scss'
 
-interface SubscribeButtonProps {
-    priceId: string;
-}
-
-export function SubscribeButton({priceId}: SubscribeButtonProps) {
-    const session = useSession();
+export function SubscribeButton() {
+    const [session] = useSession()
+    const router = useRouter()
 
     async function handleSubscribe() {
-
-        if(!session) {
+        if (!session) {
             signIn('github');
             return;
         }
 
+        if (session.activeSubscription) {
+            router.push('/posts')
+
+            return;
+        }
+
         try {
-            const response = await api.post("/subscribe");
+            const response = await api.post('/subscribe');
 
             const { sessionId } = response.data;
 
             const stripe = await getStripeJs();
 
-            await stripe.redirectToCheckout({ sessionId });
-            
-        }catch (err) {
+            stripe.redirectToCheckout({
+                sessionId
+            })
+        } catch (err) {
             alert(err.message);
         }
-
-        
     }
 
     return (
-        <button 
+        <button
             type="button"
             className={styles.subscribeButton}
-            onClick={handleSubscribe}
+            onClick={() => handleSubscribe()}
         >
-            Subscribe Now
+            Subscribe now
         </button>
     )
 }
